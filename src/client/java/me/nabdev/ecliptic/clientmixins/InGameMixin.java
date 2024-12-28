2c77f4fb2c748c184023ea13d75c65df5427223c
nab138 <nab@nabdev.me>
2024-12-28 17:49:04 -0500

    Update puzzle & bb rendering

--- src/client/java/me/nabdev/ecliptic/clientmixins/InGameMixin.java
+++ src/client/java/me/nabdev/ecliptic/clientmixins/InGameMixin.java
@@ -1,5 +1,6 @@
 package me.nabdev.ecliptic.clientmixins;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -23,13 +24,17 @@ public abstract class InGameMixin {
 
     @Shadow private static Player localPlayer;
 
+    @Unique
+    private final static float ecliptic$lineWidth = 5f;
+
+
     @Inject(method = "render", at= @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/ui/UI;render()V"))
     public void drawShaperBoundingBox(CallbackInfo ci) {
         ItemStack selected = UI.hotbar.getSelectedItemStack();
-        if(selected == null) return;
-        if(!(selected.getItem() instanceof SpatialManipulator manipulator)) return;
+        if (selected == null) return;
+        if (!(selected.getItem() instanceof SpatialManipulator manipulator)) return;
         BoundingBox bb = manipulator.getVisualBoundingBox(selected, localPlayer.getZone());
-        if(bb == null) return;
+        if (bb == null) return;
         if (sr == null) {
             sr = new ShapeRenderer();
         }
@@ -38,13 +43,14 @@ public abstract class InGameMixin {
         sr.begin(ShapeRenderer.ShapeType.Filled);
         sr.setColor(new Color(0, 0, 1, 0.25f));
         sr.box(bb.min.x, bb.min.y, bb.min.z + bb.getDepth(), bb.getWidth(), bb.getHeight(), bb.getDepth());
+        sr.box(bb.max.x, bb.max.y, bb.max.z - bb.getDepth(), -bb.getWidth(), -bb.getHeight(), -bb.getDepth());
         sr.end();
 
+        Gdx.gl.glLineWidth(ecliptic$lineWidth);
         sr.setProjectionMatrix(rawWorldCamera.combined);
         sr.begin(ShapeRenderer.ShapeType.Line);
-        sr.setColor(Color.BLUE);
+        sr.setColor(new Color(0.2352941176f, 0.2352941176f, 1, 1));
         sr.box(bb.min.x, bb.min.y, bb.min.z + bb.getDepth(), bb.getWidth(), bb.getHeight(), bb.getDepth());
         sr.end();
     }
-
 }
